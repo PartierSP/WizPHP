efac020db0e56f113969e0d9a0c665ce1c1f0741
PartierSP <mikevl1973@gmail.com>
2023-12-06 22:16:36 -0500

    Prepare extracted data for storage

--- discover.php
+++ discover.php
@@ -3,13 +3,32 @@
 include 'inc_header.php';
 
 //Get list of all local IP address complete with their MAC address
-$maclist=`arp -a`;
+$temp=explode("\n",`arp -an`);
+foreach($temp as $t){
+	$maclist[]=explode(' ',$t);
+}
+$i=0;
+foreach($maclist as $t){
+	$maclist[$i][1]=str_replace(array('(',')'),'',$maclist[$i][1]);
+	$maclist[$i][3]=str_replace(':','',$maclist[$i][3]);
+	$i++;
+}
 
 //Get list of all Wiz devices available to us
-$wizlist=`echo '{"method":"getPilot","params":{}}' | socat - UDP-DATAGRAM:255.255.255.255:38899,broadcast`;
-
-echo "$maclist<br>";
-echo $wizlist;
+$temp=`echo '{"method":"getPilot","params":{}}' | socat - UDP-DATAGRAM:255.255.255.255:38899,broadcast`;
+$temp=explode('{"method":"getPilot","env":"pro","result":{',$temp);
+foreach($temp as $t){
+	$t=str_replace(array('}','"'),'',$t);
+	$stack=explode(',',$t);
+	foreach($stack as $s_row){
+		$stack_temp[]=explode(':',$s_row);
+	}
+	$wizlist[]=$stack_temp;
+}
+echo '<pre>';
+print_r($maclist);
+print_r($wizlist);
+echo '</pre>';
 
 //Check database for new devices; Record capabilities and current status
 
